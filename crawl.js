3d19163cc83d90fd4ea1d4aa70af055e86d7987a
Guru Saran M <gurusaranm0025@gmail.com>
2024-06-07 20:27:54 +0530

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,17 +1,38 @@
-const {JSDOM} = require("jsdom")
+const { JSDOM } = require("jsdom")
+
+async function crawlPage(currentURL) {
+    console.log(`currently crawling ${currentURL}`)
+    try {
+        const resp = await fetch(currentURL)
+        if (resp.status > 399) {
+            console.error(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return
+        }
+
+        const contentType = resp.headers.get("content-type")
+        if (!contentType.includes("text/html")) {
+            console.error(`non html response, content type: ${contentType}, on page: ${currentURL}`)
+            return
+        }
+
+        console.log(await resp.text())
+    } catch (err) {
+        console.log(`error in fech: ${err.message}, on page: ${currentURL}`)
+    }
+}
 
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = []
     const dom = new JSDOM(htmlBody)
     const linkElements = dom.window.document.querySelectorAll('a')
     for (const linkElement of linkElements) {
-        
-        const urlStr = linkElement.href.slice(0,1) === '/' ? `${baseURL}${linkElement.href}` : linkElement.href
-        
+
+        const urlStr = linkElement.href.slice(0, 1) === '/' ? `${baseURL}${linkElement.href}` : linkElement.href
+
         try {
             const urlObj = new URL(urlStr)
             urls.push(urlObj.href)
-        } catch(err) {
+        } catch (err) {
             console.log(`error with url: "${urlStr}" with message: "${err.message}"`)
         }
     }
@@ -24,9 +45,9 @@ function normalizeURL(urlString) {
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
 
     if (hostPath.length > 0 && hostPath.slice(-1) == '/') {
-        return hostPath.slice(0,-1)
+        return hostPath.slice(0, -1)
     }
     return hostPath
 }
 
-module.exports = {normalizeURL, getURLsFromHTML}
+module.exports = { normalizeURL, getURLsFromHTML, crawlPage }
--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("Hello world")
+const { crawlPage } = require("./crawl.js")
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log("no website provided")
+        process.exit(1)
+    }
+    if (process.argv.length > 3) {
+        console.error("too many command line args")
+        process.exit(1)
+    }
+
+    const baseURL = process.argv[2]
+
+    console.log(`Starting crawl ${baseURL}`)
+
+    crawlPage(baseURL)
+}
+
+main()
